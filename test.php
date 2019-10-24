724879290588a6049993b56c41ed77379b85199c
Edhamza <h.eddahmouni@upm.ac.ma>
2019-10-24 10:25:04 +0100

    first commit

---
+++ test.php
@@ -0,0 +1,11 @@
+
+<?php 
+$array = array("1" => "PHP code tester Sandbox Online",  
+"foo" => "bar", 5 , 5 => 89009, 
+"Base" => "Random Stuff: " . rand(100,999),
+"PHP Version" => phpversion()
+);
+$test = array(1,2,3,4);
+var_dump($test);
+var_dump($array);
+?>
